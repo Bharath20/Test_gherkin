c87f15ad9664118199d2f47c70191aaeea344e01
Bharath <bharathk.nair10@gmail.com>
2020-08-03 02:57:56 +0530

    Changed time

--- Test_cucumber/src/main/java/com/Sample/stepDefinition/Stepdef_Login.java
+++ Test_cucumber/src/main/java/com/Sample/stepDefinition/Stepdef_Login.java
@@ -64,7 +64,7 @@ public class Stepdef_Login extends SeleniumBase {
 	@When("^the user clicks on the appoinments field$")
 	public void the_user_clicks_on_the_appoinments_field() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		maxwait(5000);
+		maxwait(6000);
 	    med.appoinment_field();
 	}
 
